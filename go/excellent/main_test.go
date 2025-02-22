d6b987022fdd094f7b7db8219d69d8439a59641f
konohiro <violet88245@gmail.com>
2025-02-22 22:59:48 +0900

    fix main_test.go

--- go/excellent/main_test.go
+++ go/excellent/main_test.go
@@ -2,7 +2,7 @@ package main
 
 import "testing"
 
-func TestEvenOrOdd(t*testing.T){
+func TestEvenOrOdd(t *testing.T){
 	result := EvenOrOdd(10)
 	if result != "even" {
 		t.Eeeorf("expected: even, actual: %s", result)
